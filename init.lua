3d829126c13af8c817ed815fee187fde59c3ec9b
Matthew <mcorbett16bf@gmail.com>
2026-04-22 21:11:32 -0400

    Adding netrw and whichkey stuff

--- init.lua
+++ init.lua
@@ -8,6 +8,8 @@ vim.g.mapleader = ' '
 vim.g.localleader = ' '
 
 require('config.options')
+require('config.netrw')
+require('config.keymaps')
 
 
 -- Folding: requires treesitter available at runtime; safe fallback if not
@@ -41,41 +43,6 @@ elseif is_linux then
   }
 end
 
--- ================================================================================================
--- KEYMAPS
--- ================================================================================================
-
--- Page Navigation
-vim.keymap.set('n', '<leader>e', ":Explore<CR>", { desc = 'File explorer' })
-vim.keymap.set("n", "n", "nzzzv", { desc = "Next search result (centered)" })
-vim.keymap.set("n", "N", "Nzzzv", { desc = "Previous search result (centered)" })
-vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = "Half page down (centered)" })
-vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = "Half page up (centered)" })
-vim.keymap.set("n", "<leader>bn", ":bnext<CR>", { desc = "Next buffer" })
-vim.keymap.set("n", "<leader>bp", ":bprevious<CR>", { desc = "Previous buffer" })
-
--- Quality of Life
-vim.keymap.set("x", "<leader>p", '"_dP', { desc = "Paste without yanking" })
-vim.keymap.set({ "n", "v" }, "<leader>x", '"_d', { desc = "Delete without yanking" })
-vim.keymap.set("n", "<A-j>", ":m .+1<CR>==", { desc = "Move line down" })
-vim.keymap.set("n", "<A-k>", ":m .-2<CR>==", { desc = "Move line up" })
-vim.keymap.set("v", "<A-j>", ":m '>+1<CR>gv=gv", { desc = "Move selection down" })
-vim.keymap.set("v", "<A-k>", ":m '<-2<CR>gv=gv", { desc = "Move selection up" })
-vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
-vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
-vim.keymap.set("n", "J", "mzJ`z", { desc = "Join lines and keep cursor position" })
-
--- Window Navigation
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to left window" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to bottom window" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to top window" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to right window" })
-vim.keymap.set("n", "<leader>sv", ":vsplit<CR>", { desc = "Split window vertically" })
-vim.keymap.set("n", "<leader>sh", ":split<CR>", { desc = "Split window horizontally" })
-vim.keymap.set("n", "<C-Up>", ":resize +2<CR>", { desc = "Increase window height" })
-vim.keymap.set("n", "<C-Down>", ":resize -2<CR>", { desc = "Decrease window height" })
-vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { desc = "Decrease window width" })
-vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { desc = "Increase window width" })
 
 require('config.lazy')
 
---
+++ lua/config/netrw.lua
@@ -0,0 +1,6 @@
+vim.g.netrw_banner = 0    -- Disable Banner
+vim.g.netrw_liststyle = 3 -- TreeView
+vim.g.netrw_winsize = 25
+-- vim.g.netrw_browse_split = 4
+
+vim.keymap.set('n', '<leader>e', ':Lexplore<CR>', { silent = true })
